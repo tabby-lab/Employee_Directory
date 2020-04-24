821f0eab4cc9adb917099fae19b873beb03bddc2
Tabby Garcia <tabby.garcia1@gmail.com>
2020-04-23 19:01:15 -0500

    Created a utils folder for my api.Using api to get random people for my directory.Created search input button using handleinputchange.Fixed my table to a bootstrap table.Installed axios.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Table from './components/Table'
+import Api from './utils/api.js'
+import Search from './components/Search'
+import api from './utils/api.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Table />
-    </div>
-  );
+class App extends Component {
+state = {
+  employeeList: [],
+  originalList:[],
+  search:""
+}
+
+handleInputChange=event=>{
+      const{name,value} = event.target
+      this.setState({
+        [name]:value  //search: "phil"
+      })
+      console.log('value', value)
+}
+
+componentDidMount(){
+   Api.getRandomPeople().then(results=>{
+     console.log("hello", results)
+     this.setState({
+       employeeList:results.data.results,
+       originalList:results.data.results
+     })
+   })
+}
+
+  render(){
+    return (
+      <div className="App">
+        <Search   search= {this.state.search}  handleInputChange={this.handleInputChange} />
+        <Table employeeList={this.state.employeeList} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
     return (
-        <div>
-            search
+        <div className="container">
+            search: <input type ="text" name= "search"  value= {props.search} onChange={props.handleInputChange} className="form-control" />
         </div>
     )
 }
